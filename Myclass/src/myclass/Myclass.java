3ae9b6bb0b7cd19337ec198a06015c144f959030
Libuseng Tsolo <libusengtsolo98@gmail.com>
2022-03-05 06:05:41 -0800

    changed

--- Myclass/src/myclass/Myclass.java
+++ Myclass/src/myclass/Myclass.java
@@ -1,19 +1,17 @@
 
 package myclass;
 class Fisrtclass{
+	
     public static void speak() throws Exception{
-    System.out.println("one") ;   
+		System.out.println("one") ;   
     }
 }
 
 public class Myclass extends Fisrtclass {
-  /*  public static void speak() {
-       System.out.println("second");
-    }*/
-
+  
     public static void main(String[] args) throws Exception {
-     Fisrtclass ab=new Fisrtclass();
-     ab.speak();
+		 Fisrtclass ab=new Fisrtclass();
+		 ab.speak();
     }
     
 }
